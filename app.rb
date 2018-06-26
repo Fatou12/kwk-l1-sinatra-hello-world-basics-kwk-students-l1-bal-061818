764155c7f4d899e482b9643e4f253de71d88b48d
Fatou12 <fatoumatadiallo12222@gmail.com>
2018-06-26 15:38:23 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,5 +1,11 @@
 class App < Sinatra::Base
 
   # Define your GET '/' route below and respond with "Hello, World!"
-
+  get '/' do
+"I like food"
 end
+  get '/french'
+"I speak french really good"
+end
+  get '/chocalte'
+"I don't like chocalte "
